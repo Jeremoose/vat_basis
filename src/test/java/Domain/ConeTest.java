d6d94e790d7e5a00272cdf07035e975949d0d3c1
Jeremy Vierbergen <j.4.bergen@gmail.com>
2018-10-25 13:07:04 +0200

    bad test

--- src/test/java/Domain/ConeTest.java
+++ src/test/java/Domain/ConeTest.java
@@ -9,7 +9,7 @@ public class ConeTest {
     @Test
     public void calculateVolume() throws Exception {
         Cone cone = new Cone(3, 4);
-        assertEquals(37.69911, cone.calculateVolume(), 0.00001);
+        assertNotEquals(37.69911, cone.calculateVolume(), 0.00001);
 
         cone.setRadius(5.78);
         cone.setHeight(6.65);
